8b8e4408a0a5c5f28460e75e606d16ab0a169985
Mohammed Shekh <mohammedshekh.820@gmail.com>
2018-10-18 10:38:25 +0100

    Updated the files

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
 function setup() {
     createCanvas(400, 400)
-    background(255)
   }
   
   function draw() {
+    
+    background(255)
     fill(255, 130, 0)
     stroke(0)
-    triangle(0, 30, 40, 0, 80, 30)
-    square(5, 30, 70, 70)
+    
+    roof(mouseX, mouseY+30, mouseX+40, mouseY, mouseX+80, mouseY+30)
+    house(mouseX+5, mouseY+30, 70)
+
+    roof(mouseX+80, mouseY+30, mouseX+120, mouseY, mouseX+160, mouseY+30)
+    house(mouseX+85, mouseY+30, 70)
+
+    roof(mouseX+160, mouseY+30, mouseX+200, mouseY, mouseX+240, mouseY+30)
+    house(mouseX+165, mouseY+30, 70)
   }
 
-  function square(x, y, size) {
+  function roof(x1, y1, x2, y2, x3, y3) {
+    triangle(x1, y1, x2, y2, x3, y3)
+  }
+
+  function house(x, y, size) {
     rect(x, y, size, size)
   }
